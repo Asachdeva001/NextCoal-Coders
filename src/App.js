96e9899b3af73239ddaa23e01a23bcdb5ac23072
Asachdeva001 <143012371+Asachdeva001@users.noreply.github.com>
2024-09-19 15:37:26 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
+import LoginSignup from './Components/LoginSignup';
+import Dashboard from './Components/Dashboard';
+import Page from './Components/Page';
+import Miningact from './Components/miningact';
+import Footer from './Components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+          <Route exact path='/loginsignup' element={<LoginSignup />} />
+          <Route exact path='/dashboard' element={<Dashboard />} />
+          <Route exact path='/dashboard' element={<Page />} />
+          <Route exact path='/miningact' element={<Miningact />} />
+        </Routes>
+        <Footer/>
+      </Router>
+    </>
   );
 }
 
